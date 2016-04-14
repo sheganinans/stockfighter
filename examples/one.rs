4c3adfad011028cffb4cde120c3f633f95faaca3
Aistis Raulinaitis <sheganinans@gmail.com>
2016-04-14 15:32:17 -0700

    example formatting

--- examples/one.rs
+++ examples/one.rs
@@ -23,10 +23,10 @@ fn main() {
   let mut one = quotes_ws!(Account(account.to_string()), Venue(venue.to_string())); // Open quotes Web Socket
   for message in one.incoming_messages() {
     let message: Message = match message { Ok(message) => message, Err(e) => { println!("Error: {:?}", e); break; }};
-	  match message.opcode {
-	    Type::Text => { match serde_json::from_str::<QuoteWS>(&bytes_to_string(&*message.payload).unwrap()) {
+    match message.opcode {
+      Type::Text => { match serde_json::from_str::<QuoteWS>(&bytes_to_string(&*message.payload).unwrap()) {
         Ok(o) => println!("{:#?}",Ok(o)), // If Quote parses correctly, print it.
         Err(_) => println!("{:#?}",       // If it doesn't, try parsing it at an ErrMsg.
                            serde_json::from_str::<ErrMsg>(&bytes_to_string(&*message.payload).unwrap())) }}
-	    _ => () }}}
+      _ => () }}}
  
